1338cfa12d5695f5b9af8528127ffd72f11d7bef
Justin A. Parado <japarado@student.apc.edu.ph>
2018-02-02 01:19:52 +0800

    Updated User databas seeds

--- Application/database/seeds/UserTableSeeder.php
+++ Application/database/seeds/UserTableSeeder.php
@@ -22,7 +22,17 @@ class UserTableSeeder extends Seeder
             [
                 'name' => 'Nicole',
                 'email' => 'nicole@mail.com',
-                'password' => bcrypt('nicnic'),
+                'password' => bcrypt('password'),
+                'user_type' => 'HR',
+                'created_at' => now(),
+                'updated_at' => now(),
+
+            ]);
+        DB::table('users')->insert(
+            [
+                'name' => 'Jason',
+                'email' => 'jason@mail.com',
+                'password' => bcrypt('password'),
                 'user_type' => 'HR',
                 'created_at' => now(),
                 'updated_at' => now(),
