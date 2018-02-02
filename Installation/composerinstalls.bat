f2a4ac222ea831f4f35611a641621d659c5b9f47
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-02-02 20:23:21 +0800

    Command prompt commands for installing and launching

---
+++ Installation/composerinstalls.bat
@@ -0,0 +1,3 @@
+cd ../application
+composer require "laravelcollective/html":"^5.4.0"
+composer require unisharp/laravel-ckeditor
---
+++ launcher.bat
@@ -0,0 +1,3 @@
+cd application
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -incognito --new-window http://localhost:8000
+php artisan serve
