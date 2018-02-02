feb48789aeb7503256564d54f714cfad557f27d9
Justin A. Parado <japarado@student.apc.edu.ph>
2018-02-02 22:45:00 +0800

    LOCATION table migration

---
+++ Application/database/migrations/2018_02_02_143754_create_location_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('location', function (Blueprint $table) {
+            $table->increments('location_id');
+            $table->integer('faculty_id')->unsigned();
+            $table->foreign('faculty_id')->references('faculty_id')->on('faculty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('location');
+    }
+}
