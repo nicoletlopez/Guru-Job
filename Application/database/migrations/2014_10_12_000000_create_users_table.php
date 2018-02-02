82b18c3fef6e86205c1f66807858bd7ab6a956ca
Justin A. Parado <japarado@student.apc.edu.ph>
2018-02-02 21:42:41 +0800

    Added default value for attribute user_type + made it nullable

--- Application/database/migrations/2014_10_12_000000_create_users_table.php
+++ Application/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('user_id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('password');
-            $table->string('user_type');
+            $table->string('user_type')->default('FACULTY')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
