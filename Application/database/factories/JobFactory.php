770b52b42e4094d03c62a2a88c59907d6140ab62
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-02-09 01:51:55 +0800

    Adjusted Seeds Based on DB Changes

--- Application/database/factories/JobFactory.php
+++ Application/database/factories/JobFactory.php
@@ -6,8 +6,6 @@ $factory->define(App\Job::class, function (Faker $faker) {
     return [
         'title' => 'Hiring '.$faker->sentence(3),
         'desc' => $faker->paragraphs(3,true),
-        'start_time' => $faker->time($format = 'H:i:s', $max = '12:00:00'), // '20:49:42'
-        'end_time' => $faker->time($format = 'H:i:s', $max = '19:30:00'), // '20:49:42'
         'salary' => $faker->randomFloat($nbMaxDecimals = 2, $min = 10000, $max = 50000) // 48.8932
     ];
 });
--- Application/database/seeds/JobTableSeeder.php
+++ Application/database/seeds/JobTableSeeder.php
@@ -24,44 +24,9 @@ class JobTableSeeder extends Seeder
                 $type = 'Full-Time';
             }
 
-            $b = rand(0, 9);
-            switch ($b) {
-                case 0:
-                    $wd = 'Mon, Thu';
-                    break;
-                case 1:
-                    $wd = "Tue, Thu";
-                    break;
-                case 2:
-                    $wd = 'Mon, Wed, Fri';
-                    break;
-                case 3:
-                    $wd = "Mon, Tue, Thu, Fri";
-                    break;
-                case 4:
-                    $wd = 'Mon';
-                    break;
-                case 5:
-                    $wd = "Tue";
-                    break;
-                case 6:
-                    $wd = 'Wed';
-                    break;
-                case 7:
-                    $wd = "Sat";
-                    break;
-                case 8:
-                    $wd = 'Mon, Thu, Sat';
-                    break;
-                default:
-                    $wd = 'TBA';
-                    break;
-            }
-
             factory(App\Job::class)->create([
                 'user_id' => $hr->user_id,
                 'type' => $type,
-                'work_days' => $wd,
             ]);
         }
     }
