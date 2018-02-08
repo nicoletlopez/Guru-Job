3a4362fcfff63be5711616091745cc6d70ac11a1
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-02-08 22:39:20 +0800

    Jobs in DB can be seen w/ pagination

--- Application/app/Http/Controllers/PagesController.php
+++ Application/app/Http/Controllers/PagesController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Job;
 class PagesController extends Controller
 {
     public function index(){
-        return view('pages.home');
+        $context = array(
+            'jobs' => Job::orderBy('title')->paginate(4),
+        );
+        return view('pages.home')->with($context);
     }
 }
--- Application/resources/views/layouts/jobs.blade.php
+++ Application/resources/views/layouts/jobs.blade.php
@@ -7,23 +7,28 @@
 
         <div class="row">
             <div class="col-md-12">
+                @if(count($jobs)>0)
+                @foreach($jobs as $job)
                 <div class="job-list">
                     <div class="thumb">
                         <a href="job-details"><img src="{{asset('img/jobs/img-1.jpg')}}" alt=""></a>
                     </div>
                     <div class="job-list-content">
-                        <h4><a href="job-details">Need a web designer</a><span class="full-time">Full-Time</span>
+                        <h4><a href="job-details">{{$job->title}}</a>
+                            @if($job->type == 'Full-Time')
+                                <span class="full-time">Full-Time</span>
+                            @else
+                                <span class="part-time">Part-Time</span>
+                            @endif
                         </h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quaerat aut veniam molestiae
-                            atque dolorum omnis temporibus consequuntur saepe. Nemo atque consectetur saepe corporis
-                            odit in dicta reprehenderit, officiis, praesentium?</p>
+                        <p>{{$job->desc}}</p>
                         <div class="job-tag">
                             <div class="pull-left">
                                 <div class="meta-tag">
-                                    <span><a href="browse-categories"><i class="ti-brush"></i>Art/Design</a></span>
-                                    <span><i class="ti-location-pin"></i>Washington, USA</span>
-                                    <span><i class="ti-time"></i>7:00 a.m. - 10:00 a.m.</span>
-                                    <span><i class="ti-world"></i>Mon, Wed, Fri</span>
+                                    <!--<span><a href="#"><i class="ti-brush"></i>Art/Design</a></span>-->
+                                    <span><i class="ti-location-pin"></i>LOCATION</span>
+                                    <span><i class="ti-time"></i>{{date("g:i a",strtotime($job->start_time))}} - {{date("g:i a",strtotime($job->end_time))}}</span>
+                                    <span><i class="ti-world"></i>{{$job->work_days}}</span>
                                 </div>
                             </div>
                             <div class="pull-right">
@@ -35,92 +40,9 @@
                         </div>
                     </div>
                 </div>
-                <div class="job-list">
-                    <div class="thumb">
-                        <a href="job-details"><img src="{{asset('img/jobs/img-2.jpg')}}" alt=""></a>
-                    </div>
-                    <div class="job-list-content">
-                        <h4><a href="job-details">Front-end developer needed</a><span
-                                    class="full-time">Full-Time</span></h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quaerat aut veniam molestiae
-                            atque dolorum omnis temporibus consequuntur saepe. Nemo atque consectetur saepe corporis
-                            odit in dicta reprehenderit, officiis, praesentium?</p>
-                        <div class="job-tag">
-                            <div class="pull-left">
-                                <div class="meta-tag">
-                                    <span><a href="browse-categories"><i
-                                                    class="ti-desktop"></i>Technologies</a></span>
-                                    <span><i class="ti-location-pin"></i>Cupertino, CA, USA</span>
-                                    <span><i class="ti-time"></i>7:00 a.m. - 10:00 a.m.</span>
-                                    <span><i class="ti-world"></i>Mon, Wed, Fri</span>
-                                </div>
-                            </div>
-                            <div class="pull-right">
-                                <div class="icon">
-                                    <i class="ti-heart"></i>
-                                </div>
-                                <a href="job-details" class="btn btn-common btn-rm">More Detail</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="job-list">
-                    <div class="thumb">
-                        <a href="job-details"><img src="{{asset('img/jobs/img-3.jpg')}}" alt=""></a>
-                    </div>
-                    <div class="job-list-content">
-                        <h4><a href="job-details">Senior Accountant</a><span class="part-time">Part-Time</span>
-                        </h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quaerat aut veniam molestiae
-                            atque dolorum omnis temporibus consequuntur saepe. Nemo atque consectetur saepe corporis
-                            odit in dicta reprehenderit, officiis, praesentium?</p>
-                        <div class="job-tag">
-                            <div class="pull-left">
-                                <div class="meta-tag">
-                                    <span><a href="browse-categories"><i class="ti-home"></i>Finance</a></span>
-                                    <span><i class="ti-location-pin"></i>Delaware, USA</span>
-                                    <span><i class="ti-time"></i>7:00 a.m. - 10:00 a.m.</span>
-                                    <span><i class="ti-world"></i>Mon, Wed, Fri</span>
-                                </div>
-                            </div>
-                            <div class="pull-right">
-                                <div class="icon">
-                                    <i class="ti-heart"></i>
-                                </div>
-                                <a href="job-details" class="btn btn-common btn-rm">More Detail</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="job-list">
-                    <div class="thumb">
-                        <a href="job-details"><img src="{{asset('img/jobs/img-4.jpg')}}" alt=""></a>
-                    </div>
-                    <div class="job-list-content">
-                        <h4><a href="job-details">Fullstack web developer needed</a><span class="full-time">Full-Time</span>
-                        </h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quaerat aut veniam molestiae
-                            atque dolorum omnis temporibus consequuntur saepe. Nemo atque consectetur saepe corporis
-                            odit in dicta reprehenderit, officiis, praesentium?</p>
-                        <div class="job-tag">
-                            <div class="pull-left">
-                                <div class="meta-tag">
-                                    <span><a href="browse-categories"><i
-                                                    class="ti-desktop"></i>Technologies</a></span>
-                                    <span><i class="ti-location-pin"></i>New York, USA</span>
-                                    <span><i class="ti-time"></i>7:00 a.m. - 10:00 a.m.</span>
-                                    <span><i class="ti-world"></i>Mon, Wed, Fri</span>
-                                </div>
-                            </div>
-                            <div class="pull-right">
-                                <div class="icon">
-                                    <i class="ti-heart"></i>
-                                </div>
-                                <a href="job-details" class="btn btn-common btn-rm">More Detail</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
+                    {{$jobs->links()}}
+                @endif
             </div>
         </div>
     </div>
