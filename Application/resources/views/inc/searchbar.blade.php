fff34c00b149f3e61dca506a0588d6111dcec6ca
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-02-09 23:33:11 +0800

    commented out  nonfunctional fields in searchbar

--- Application/resources/views/inc/searchbar.blade.php
+++ Application/resources/views/inc/searchbar.blade.php
@@ -4,11 +4,12 @@
             <div class="content">
                 <form action="search" method="get">
                     <div class="row">
-                        <div class="col-md-4 col-sm-6">
+                        <div class="col-md-11 col-sm-6">
                             <div class="form-group">
-                                <input class="form-control" type="text" name="s" placeholder="job title / keywords / company name">
+                                <input class="form-control" type="text" name="s" placeholder="job title / keywords">
                             </div>
                         </div>
+                        <!--
                         <div class="col-md-4 col-sm-6">
                             <div class="search-category-container">
                                 <label class="styled-select">
@@ -37,6 +38,7 @@
                                 </label>
                             </div>
                         </div>
+                        -->
                         <div class="col-md-1 col-sm-6">
                             <button type="submit" class="btn btn-search-icon"><i class="ti-search"></i></button>
                         </div>
