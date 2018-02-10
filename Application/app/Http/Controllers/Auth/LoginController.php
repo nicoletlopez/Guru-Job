6d89cd68b6a8bbbeb8e1cb3a1bb00e674cebf812
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-02-10 12:01:27 +0800

    Override logout function
    Co-Authored-By: Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>

--- Application/app/Http/Controllers/Auth/LoginController.php
+++ Application/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -27,6 +28,13 @@ class LoginController extends Controller
      * @var string
      */
 
+    protected function logout(Request $request){
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        return redirect(route('hr-dashboard'));
+    }
 
     //protected $redirectTo = '/dashboard';
     protected function authenticated($request, $user)
