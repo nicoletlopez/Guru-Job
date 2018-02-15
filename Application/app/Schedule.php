617afc64beb4a49a7b898f42c058fc005831a2e1
Justin A. Parado <japarado@student.apc.edu.ph>
2018-02-15 21:19:41 +0800

    SUBJECT and SCHEDULE models

---
+++ Application/app/Schedule.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+    //
+    protected $table = 'schedule';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    public function subject()
+    {
+        return $this->belongsTo('App\Subject','subject_id','id');
+    }
+}
--- Application/app/Subject.php
+++ Application/app/Subject.php
@@ -26,4 +26,9 @@ class Subject extends Model
         return $this->belongsToMany('App\Skill','subject_requires_skill','subject_id','skill_id')
             ->using('App\SubjectRequiresSkill');
     }
+
+    public function schedules()
+    {
+        return $this->hasMany('App\Schedule','subject_id','id');
+    }
 }
