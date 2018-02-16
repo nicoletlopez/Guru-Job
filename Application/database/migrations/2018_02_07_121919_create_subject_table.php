5298df96f8b7163ce3803d7bdaca044fcbc89868
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-02-16 17:39:52 +0800

    Revert "Deleted some columns from SUBJECT table"
    This reverts commit 7a1111553d61d244ad6921694de2d0fa07368cd8.

--- Application/database/migrations/2018_02_07_121919_create_subject_table.php
+++ Application/database/migrations/2018_02_07_121919_create_subject_table.php
@@ -19,6 +19,9 @@ class CreateSubjectTable extends Migration
             $table->integer('job_id')->unsigned()->nullable();
             $table->string('name');
             $table->text('desc');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->integer('days');
 
             $table->foreign('user_id')->references('user_id')->on('hr');
             $table->foreign('job_id')->references('id')->on('job');
