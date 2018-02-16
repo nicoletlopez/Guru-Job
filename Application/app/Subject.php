cdb975e4a40aac521522ba34e58a55ada43069c3
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-02-16 17:38:42 +0800

    Revert "Created SCHEDULE table migration and model. Established model relationships for SUBJECT and SCHEDULE. Atttributes for SCHEDULE incomplete"
    This reverts commit abf02eec7aec62c5594c86505ecb5d5d236485d7.

--- Application/app/Subject.php
+++ Application/app/Subject.php
@@ -26,9 +26,12 @@ class Subject extends Model
         return $this->belongsToMany('App\Skill','subject_requires_skill','subject_id','skill_id')
             ->using('App\SubjectRequiresSkill');
     }
+<<<<<<< HEAD
 
     public function schedules()
     {
         return $this->hasMany('App\Schedule','subject_id','id');
     }
+=======
+>>>>>>> parent of abf02ee... Created SCHEDULE table migration and model. Established model relationships for SUBJECT and SCHEDULE. Atttributes for SCHEDULE incomplete
 }
