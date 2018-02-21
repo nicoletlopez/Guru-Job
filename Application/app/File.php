40ce5645bdd66d2c94f3cdd4a4f15595e97621cc
Justin A. Parado <japarado@student.apc.edu.ph>
2018-02-21 21:48:31 +0800

    renamed faculties() function in File.php to owners()

--- Application/app/File.php
+++ Application/app/File.php
@@ -9,7 +9,7 @@ class File extends Model
     //
     protected $table = 'file';
 
-    public function faculties()
+    public function owners()
     {
         return $this->belongsToMany(Faculty::class,'faculty_has_file','file_id','user_id');
     }
