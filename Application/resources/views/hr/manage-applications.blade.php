19d26bf85c88c78a290f35fb31799370052f3be9
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-02-23 23:52:34 +0800

    fixed applications UI

--- Application/resources/views/hr/manage-applications.blade.php
+++ Application/resources/views/hr/manage-applications.blade.php
@@ -7,144 +7,24 @@
 @section('dashboard-content')
     <div class="job-alerts-item candidates">
         <h3 class="alerts-title">Manage applications</h3>
-        <div class="alerts-list">
-            <div class="row">
-                <div class="col-md-5">
-                    <p>Job Title</p>
-                </div>
-                <div class="col-md-2">
-                    <p>Applicant</p>
-                </div>
-                <div class="col-md-2">
-                    <p>Date Applied</p>
-                </div>
-                <div class="col-md-3">
-                    <p>Resume</p>
-                </div>
-            </div>
-        </div>
-        @foreach($applicants as $applicant)
-            <div class="applications-content">
-                <div class="row">
-                    <div class="col-md-5">
-                        <div class="thums">
-                            <img src="{{asset('img/jobs/img-1.jpg')}}" alt="">
-                        </div>
-                        <h3>{{$applicant->pivot}}</h3>
-                        <span>Quick Studio</span>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="can-img"><a href="#"><img src="{{asset('img/jobs/candidates.png')}}" alt=""></a>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <p>Nov 14th, 2017</p>
-                    </div>
-                    <div class="col-md-3">
-                        <p>Resume.docx</p>
-                    </div>
-                </div>
-            </div>
-        @endforeach
-            {{--<div class="applications-content">
-                 <div class="row">
-                     <div class="col-md-5">
-                         <div class="thums">
-                             <img src="{{asset('img/jobs/img-1.jpg')}}" alt="">
-                         </div>
-                         <h3>Front-end developer needed</h3>
-                         <span>Quick Studio</span>
-                     </div>
-                     <div class="col-md-2">
-                         <div class="can-img"><a href="#"><img src="{{asset('img/jobs/candidates.png')}}" alt=""></a></div>
-                     </div>
-                     <div class="col-md-2">
-                         <p>Nov 14th, 2017</p>
-                     </div>
-                     <div class="col-md-3">
-                         <p>Resume.docx</p>
-                     </div>
-                 </div>
-             </div>
-             <div class="applications-content">
-                 <div class="row">
-                     <div class="col-md-5">
-                         <div class="thums">
-                             <img src="{{asset('img/jobs/img-1.jpg')}}" alt="">
-                         </div>
-                         <h3>We're looking for an Art Director</h3>
-                         <span>Quick Studio</span>
-                     </div>
-                     <div class="col-md-2">
-                         <div class="can-img"><a href="#"><img src="{{asset('img/jobs/candidates.png')}}" alt=""></a></div>
-                     </div>
-                     <div class="col-md-2">
-                         <p>Nov 14th, 2017</p>
-                     </div>
-                     <div class="col-md-3">
-                         <p>Resume.docx</p>
-                     </div>
-                 </div>
-             </div>
-             <div class="applications-content">
-                 <div class="row">
-                     <div class="col-md-5">
-                         <div class="thums">
-                             <img src="{{asset('img/jobs/img-1.jpg')}}" alt="">
-                         </div>
-                         <h3>Web designer needed</h3>
-                         <span>Quick Studio</span>
-                     </div>
-                     <div class="col-md-2">
-                         <div class="can-img"><a href="#"><img src="{{asset('img/jobs/candidates.png')}}" alt=""></a></div>
-                     </div>
-                     <div class="col-md-2">
-                         <p>Nov 14th, 2017</p>
-                     </div>
-                     <div class="col-md-3">
-                         <p>Resume.docx</p>
-                     </div>
-                 </div>
-             </div>
-             <div class="applications-content">
-                 <div class="row">
-                     <div class="col-md-5">
-                         <div class="thums">
-                             <img src="{{asset('img/jobs/img-1.jpg')}}" alt="">
-                         </div>
-                         <h3>Looking for a Project Leader</h3>
-                         <span>Quick Studio</span>
-                     </div>
-                     <div class="col-md-2">
-                         <div class="can-img"><a href="#"><img src="{{asset('img/jobs/candidates.png')}}" alt=""></a></div>
-                     </div>
-                     <div class="col-md-2">
-                         <p>Nov 14th, 2017</p>
-                     </div>
-                     <div class="col-md-3">
-                         <p>Resume.docx</p>
-                     </div>
-                 </div>
-             </div>
-             <div class="applications-content">
-                 <div class="row">
-                     <div class="col-md-5">
-                         <div class="thums">
-                             <img src="{{asset('img/jobs/img-1.jpg')}}" alt="">
-                         </div>
-                         <h3>We're hiring an fullstack designer</h3>
-                         <span>Quick Studio</span>
-                     </div>
-                     <div class="col-md-2">
-                         <div class="can-img"><a href="#"><img src="{{asset('img/jobs/candidates.png')}}" alt=""></a></div>
-                     </div>
-                     <div class="col-md-2">
-                         <p>Nov 14th, 2017</p>
-                     </div>
-                     <div class="col-md-3">
-                         <p>Resume.docx</p>
-                     </div>
-                 </div>
-             </div>--}}
+        <table class="table">
+            <thead class="">
+            <tr>
+                <th>Job Title</th>
+                <th>Applicant</th>
+                <th>Name</th>
+                <th>Date Applied</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><a href="#"><h3>jobTitle</h3></a></td>
+                    <td><div class="can-img"><a href="#"><img class="img-circle" src="https://lorempixel.com/640/480/?57746" alt=""></a>
+                        </div></td>
+                    <td>applicantName</td>
+                    <td>dateApplied</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 @endsection
