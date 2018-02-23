ebb67a9881b7526e4f92b4935040f1947812ecad
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-02-24 01:54:32 +0800

    minor fix on manage jobs UI

--- Application/resources/views/hr/manage-jobs.blade.php
+++ Application/resources/views/hr/manage-jobs.blade.php
@@ -13,10 +13,10 @@
             @if(count($jobs)>0)
                 <thead class="">
                 <tr>
-                    <th><p>Job Title</p></th>
-                    <th><p># of Applicants</p></th>
-                    <th><p>Applicants</p></th>
-                    <th><p>Action</p></th>
+                    <th>Job Title</th>
+                    <th># of Applicants</th>
+                    <th>Applicants</th>
+                    <th>Action</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -26,7 +26,24 @@
                         <td><a href="/jobs/{{$job->id}}"><h3>{{$job->title}}</h3></a></td>
                         <td><span class="badge">{{$job->applicants->count()}}</span></td>
                         <td>
-                            <div class="can-img"><a href="#"><img src="{{asset('/img/jobs/candidates.png')}}"></a></div>
+                            @if(count($job->applicants) > 0)
+                                <a href="#" class="btn btn-success">
+                                    View
+                                </a>
+                                <!--
+                                <div class="can-img">
+
+
+                                    @foreach($job->applicants as $applicant)
+                                        <a href="#"><img class="img-circle"
+                                                         src="{{$applicant->user->profile->picture}}"/></a>
+                                    @endforeach
+
+                                </div>
+                                -->
+                                @else
+                                <p class="text-center">None</p>
+                            @endif
                         </td>
                         <td>
                             <a href="/jobs/{{$job->id}}/edit" class="btn btn-primary">Update</a>
