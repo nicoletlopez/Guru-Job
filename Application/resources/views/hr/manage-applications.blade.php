c15e6e3c4d9fd4ae73cf56430e9330dda845330c
Justin A. Parado <japarado@student.apc.edu.ph>
2018-02-24 00:14:18 +0800

    HR can now view all applications made

--- Application/resources/views/hr/manage-applications.blade.php
+++ Application/resources/views/hr/manage-applications.blade.php
@@ -5,26 +5,34 @@
 @section('manage-applications-active') active @endsection
 
 @section('dashboard-content')
-    <div class="job-alerts-item candidates">
-        <h3 class="alerts-title">Manage applications</h3>
-        <table class="table">
-            <thead class="">
-            <tr>
-                <th>Job Title</th>
-                <th>Applicant</th>
-                <th>Name</th>
-                <th>Date Applied</th>
-            </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td><a href="#"><h3>jobTitle</h3></a></td>
-                    <td><div class="can-img"><a href="#"><img class="img-circle" src="https://lorempixel.com/640/480/?57746" alt=""></a>
-                        </div></td>
-                    <td>applicantName</td>
-                    <td>dateApplied</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+    @foreach($jobs as $job)
+        @foreach($job->applicants as $applicant)
+            <div class="job-alerts-item candidates">
+                <h3 class="alerts-title">Manage applications</h3>
+                <table class="table">
+                    <thead class="">
+                    <tr>
+                        <th>Job Title</th>
+                        <th>Applicant</th>
+                        <th>Name</th>
+                        <th>Date Applied</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td><a href="#"><h3>{{$job->title}}</h3></a></td>
+                        <td>
+                            <div class="can-img"><a href="#"><img class="img-rounded"
+                                                                  src="{{$applicant->user->profile->picture}}"
+                                                                  alt=""></a>
+                            </div>
+                        </td>
+                        <td>{{$applicant->user->name}}</td>
+                        <td>{{$applicant->pivot->created_at}}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        @endforeach
+    @endforeach
 @endsection
