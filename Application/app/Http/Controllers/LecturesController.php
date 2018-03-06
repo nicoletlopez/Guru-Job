29443a60427f891c261de9f481ea3d1b61139712
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-03-06 16:49:52 +0800

    Added validation in lectures

--- Application/app/Http/Controllers/LecturesController.php
+++ Application/app/Http/Controllers/LecturesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CreateLecture;
 use Illuminate\Http\Request;
 use App\Lecture;
+
 class LecturesController extends Controller
 {
     /**
@@ -14,10 +15,13 @@ class LecturesController extends Controller
      */
     public function index()
     {
+        if(!auth()->user()){
+            return redirect()->route('login');
+        }
         $lectures = auth()->user()->faculty->ownedLectures;
         $context =
             [
-                'lectures'=>$lectures,
+                'lectures' => $lectures,
             ];
         return view('faculty.manage-lectures')->with($context);
     }
@@ -35,41 +39,44 @@ class LecturesController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(CreateLecture $request)
     {
-        $owner=auth()->user()->id;
-        $title=$request->input('title');
-        $overview=$request->input('overview');
-        $objectives=$request->input('objectives');
+        $owner = auth()->user()->id;
+        $title = $request->input('title');
+        $overview = $request->input('overview');
+        $objectives = $request->input('objectives');
 
-        $lecture=new Lecture();
-        $lecture->owner_id=$owner;
-        $lecture->title=$title;
-        $lecture->overview=$overview;
-        $lecture->objectives=$objectives;
+        $lecture = new Lecture();
+        $lecture->owner_id = $owner;
+        $lecture->title = $title;
+        $lecture->overview = $overview;
+        $lecture->objectives = $objectives;
         $lecture->save();
 
-        return redirect('/lectures/'.$lecture->id);
+        return redirect('/lectures/' . $lecture->id);
 
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        if(!auth()->user()){
+        $lecture = Lecture::find($id);
+        if (!auth()->user()) {
             return redirect()->route('login');
+        }elseif(!$lecture) {
+            return redirect()->route('lectures.index');
         }
-        $lecture=Lecture::find($id);
-        $context=array(
-            'lecture'=>$lecture
+        $context = array(
+            'lecture' => $lecture,
+            'files'=>$lecture->files,
         );
         return view('lectures.lecture-details')->with($context);
     }
@@ -77,14 +84,14 @@ class LecturesController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
         $lecture = Lecture::find($id);
-        $context=array(
-            'lecture'=>$lecture,
+        $context = array(
+            'lecture' => $lecture,
         );
         return view('lectures.lecture-edit')->with($context);
     }
@@ -92,21 +99,21 @@ class LecturesController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(CreateLecture $request, $id)
     {
-        $lecture=Lecture::find($id);
+        $lecture = Lecture::find($id);
 
-        $title=$request->input('title');
-        $overview=$request->input('overview');
-        $objectives=$request->input('objectives');
+        $title = $request->input('title');
+        $overview = $request->input('overview');
+        $objectives = $request->input('objectives');
 
-        $lecture->title=$title;
-        $lecture->overview=$overview;
-        $lecture->objectives=$objectives;
+        $lecture->title = $title;
+        $lecture->overview = $overview;
+        $lecture->objectives = $objectives;
         $lecture->save();
 
         return redirect('lectures/' . $lecture->id);
@@ -115,14 +122,14 @@ class LecturesController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        $lecture=Lecture::find($id);
+        $lecture = Lecture::find($id);
         $lecture->delete();
-        return back()->with('success','Lecture Deleted');
+        return back()->with('success', 'Lecture Deleted');
     }
 
     public function share()
