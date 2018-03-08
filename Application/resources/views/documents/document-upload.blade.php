d9ef2f132295e97b5a188f25c255579830426799
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-03-09 01:33:29 +0800

    document files UI

---
+++ Application/resources/views/documents/document-upload.blade.php
@@ -0,0 +1,7 @@
+{!! Form::open(['class'=>'dropzone','method'=>'POST','enctype'=>'multipart/form-data']) !!}
+<div class="fallback">
+    {{Form::file('file',['multiple'])}}
+</div>
+{!! Form::close() !!}
+<br/>
+<input type="button" class="btn btn-success" value="Upload" onClick="window.location.reload()">
