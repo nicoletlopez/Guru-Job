d73bcaab18583d092b32a3fde7bb52bce3851abe
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-03-08 20:10:45 +0800

    Changed file path

--- Application/app/Http/Controllers/FilesController.php
+++ Application/app/Http/Controllers/FilesController.php
@@ -3,22 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\File;
+use App\Lecture;
 use Illuminate\Http\Request;
 use App\Http\Requests\LectureUpload;
 use Illuminate\Support\Facades\Storage;
 
 class FilesController extends Controller
 {
-
     //for lecture files
     public function lectureUpload(LectureUpload $request,$lectureId){
+
+        $lecture=Lecture::find($lectureId);
+        $userName=$lecture->faculty->user->name;
+        $name=str_replace(' ','_',strtolower($userName));
+
         $fileNameWithExt=$request->file('file')->getClientOriginalName();
         $filename=pathinfo($fileNameWithExt,PATHINFO_FILENAME);
         $extension=$request->file('file')->getClientOriginalExtension();
         $fileNameToStore=$filename.'_'.time().'.'.$extension;
         //$fileNameToStore=$filename.'.'.$extension;
         //$fileNameToStore=$request->file('file')->getClientOriginalName();
-        $path=$request->file('file')->storeAs('public/lectures/lecture_files_'.$lectureId,$fileNameToStore);
+        $path=$request->file('file')->storeAs('/public/'.$name.'/lectures/lecture_files/',$fileNameToStore);
         $file=new File;
         $file->lecture_id=$lectureId;
         $file->name=$fileNameToStore;
@@ -27,13 +32,20 @@ class FilesController extends Controller
         return back()->with('success','File uploaded.');
     }
     public function deleteLectureFile($lectureId,$id){
+        $lecture=Lecture::find($lectureId);
+        $userName=$lecture->faculty->user->name;
+        $name=str_replace(' ','_',strtolower($userName));
         $file=File::find($id);
-        Storage::delete('/public/lectures/'."lecture_files_".$lectureId."/".$file->name);
+        Storage::delete('/public/'.$name.'/lectures/lecture_files/'.$file->name);
         $file->delete();
         return back()->with('success','File Deleted.');
     }
     public function downloadLectureFile($lectureId,$fileName){
-        $file=public_path().'/storage/lectures/'."lecture_files_".$lectureId."/".$fileName;
+
+        $lecture=Lecture::find($lectureId);
+        $userName=$lecture->faculty->user->name;
+        $name=str_replace(' ','_',strtolower($userName));
+        $file=public_path().'/storage/'.$name.'/lectures/lecture_files/'.$fileName;
         $headers = array(
             'Content-Type: application/octet-stream',
         );
