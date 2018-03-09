af0356130c3695c2dee91e5d303e57b12e8eacd4
Justin Parado <japarado@student.apc.edu.ph>
2018-03-09 13:56:52 +0800

    Warning messages

--- Application/resources/views/inc/messages.blade.php
+++ Application/resources/views/inc/messages.blade.php
@@ -11,6 +11,13 @@
         {{session('success')}}
     </div>
 @endif
+
+@if(session('warning'))
+    <div class="alert alert-warning">
+        {{session('warning')}}
+    </div>
+@endif
+
 @if(session('error'))
     <div class="alert alert-danger">
         {{session('error')}}
