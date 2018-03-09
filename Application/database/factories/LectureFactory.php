2d560ef4179f7d6c318ca24801b50ad55518be13
Jade Ericson Adriano <jmadriano2@student.apc.edu.ph>
2018-03-09 17:04:58 +0800

    Made Lecture more Realistic

--- Application/database/factories/LectureFactory.php
+++ Application/database/factories/LectureFactory.php
@@ -4,8 +4,5 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Lecture::class, function (Faker $faker) {
     return [
-        'title' => $faker->sentence(4),
-        'overview' => $faker->paragraphs($nb = rand(1,2), $asText = true),
-        'objectives' => $faker->paragraphs($nb = 1, $asText = true),
     ];
 });
--- Application/database/seeds/LectureTableSeeder.php
+++ Application/database/seeds/LectureTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Faculty;
+use App\Subject;
 
 class LectureTableSeeder extends Seeder
 {
@@ -16,9 +17,12 @@ class LectureTableSeeder extends Seeder
         DB::update('UPDATE SQLITE_SEQUENCE SET seq = 0 WHERE name = "lecture";');
 
         $owners = Faculty::all();
-
         foreach ($owners as $owner) {
+            $subject = Subject::inRandomOrder()->first()->name;
             factory(App\Lecture::class)->create([
+                'title' => 'Lecture for '.$subject,
+                'overview' => 'This lecture aims to teach '.$subject.' in a comprehensive manner.',
+                'objectives' => '(1) Learn about '.$subject.' (2) Learn more about '.$subject,
                 'owner_id' => $owner->user_id,
             ]);
         }
