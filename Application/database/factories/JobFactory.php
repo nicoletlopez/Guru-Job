fe6274b7dfb0149a8c5d5b187a2992267b8c36be
Jade Ericson Adriano <jmadriano2@student.apc.edu.ph>
2018-03-09 14:36:00 +0800

    Edited JobFactory

--- Application/database/factories/JobFactory.php
+++ Application/database/factories/JobFactory.php
@@ -4,7 +4,5 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Job::class, function (Faker $faker) {
     return [
-        'floor_salary' => $faker->randomFloat($nbMaxDecimals = 2, $min = 10000, $max = 15000), // 48.8932
-        'ceiling_salary' => $faker->randomFloat($nbMaxDecimals = 2, $min = 20000, $max = 35000), // 48.8932
     ];
 });
