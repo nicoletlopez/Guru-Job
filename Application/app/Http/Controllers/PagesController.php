657d3cbf15f92640b6f9a487c290ade95084626f
Justin A. Parado <japarado@student.apc.edu.ph>
2018-03-11 10:25:47 +0800

    Configure and enable redis cache

--- Application/app/Http/Controllers/PagesController.php
+++ Application/app/Http/Controllers/PagesController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Job;
+use Illuminate\Support\Facades\Cache;
 class PagesController extends Controller
 {
     public function index(){
+
+        $jobs = Cache::remember('jobs',22*60,function ()
+        {
+           return Job::orderBy('created_at','desc')->paginate(4);
+        });
+
         $context = array(
-            'jobs' => Job::orderBy('created_at','desc')->paginate(4),
+            'jobs' => $jobs,
+            //'jobs' => Job::orderBy('created_at','desc')->paginate(4),
         );
+
         return view('pages.home')->with($context);
     }
 }
