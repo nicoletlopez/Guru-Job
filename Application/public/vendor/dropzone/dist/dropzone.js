6a9f4a78160e57f56ce812c624df16a6c4024c5c
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-03-14 15:35:40 +0800

    max file size change

--- Application/public/vendor/dropzone/dist/dropzone.js
+++ Application/public/vendor/dropzone/dist/dropzone.js
@@ -119,7 +119,7 @@
       withCredentials: false,
       parallelUploads: 2,
       uploadMultiple: false,
-      maxFilesize: 256,
+      maxFilesize: 100,
       paramName: "file",
       createImageThumbnails: true,
       maxThumbnailFilesize: 10,
