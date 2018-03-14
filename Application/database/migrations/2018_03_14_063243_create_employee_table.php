8d48a3069c7766da6f7fb88328c39379b08825f7
Justin A. Parado <japarado@student.apc.edu.ph>
2018-03-14 16:08:46 +0800

    Created employee table mirgation
    Employee is the bridge table between the many to many relationship that hr and faculty has

---
+++ Application/database/migrations/2018_03_14_063243_create_employee_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->integer('hr_id')->unsigned();
+            $table->integer('faculty_id')->unsigned();
+
+            $table->foreign('hr_id')->references('user_id')->on('hr');
+            $table->foreign('faculty_id')->references('user_id')->on('faculty');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+}
