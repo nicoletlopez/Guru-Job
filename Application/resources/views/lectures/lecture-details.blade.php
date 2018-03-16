02eccb75a43c6c1fb90cca741b9e3eb4475fa0e5
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-03-17 04:59:15 +0800

    lecture-details.blade.php now Extends lectures.blade.php Layout

--- Application/resources/views/lectures/lecture-details.blade.php
+++ Application/resources/views/lectures/lecture-details.blade.php
@@ -1,39 +1,22 @@
-@extends('faculty.dashboard-menu')
+@extends('layouts.lectures')
 @section('title')- Lecture Details @endsection
 @section('current') Lecture Details @endsection
 @section('current-header') Lecture Details @endsection
-@section('manage-lectures-active') active @endsection
 @section('tab-detail-active') active @endsection
 
-@section('dashboard-content')
-    <a href="{{route('lectures.index')}}" class="btn btn-primary"><i class="ti-arrow-left"></i> Go Back</a>
-    <hr/>
-    @include('inc.messages')
-    <section class="section job-detail well">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-9 col-sm-8">
-                    <div class="content-area">
-                        @include('lectures.lecture-tabs')
-                        <div class="box col-md-11">
-                            <div class="text-left">
-                                <h3>{{$lecture->title}}</h3>
-                            </div>
-                            <div class="clearfix">
-                                <h4>Lecture Overview</h4>
-                                {!! $lecture->overview !!}
-                            </div>
-                            <div class="clearfix">
-                                <h4>Lecture Objectives</h4>
-                                {!! $lecture->objectives !!}
-                            </div>
-                            <div class="clearfix">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </section>
+@section('active-tab-content')
+    <div class="text-left">
+        <h3>{{$lecture->title}}</h3>
+    </div>
+    <div class="clearfix">
+        <h4>Lecture Overview</h4>
+        {!! $lecture->overview !!}
+    </div>
+    <div class="clearfix">
+        <h4>Lecture Objectives</h4>
+        {!! $lecture->objectives !!}
+    </div>
+    <div class="clearfix">
+    </div>
+    </div>
 @endsection
