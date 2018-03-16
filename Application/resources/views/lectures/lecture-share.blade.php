ab54dbd808b36e947be84dbac9b4e05df503dbda
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-03-17 05:00:09 +0800

    lecture-share.blade.php Now Extends lectrues.blade.php Layout

--- Application/resources/views/lectures/lecture-share.blade.php
+++ Application/resources/views/lectures/lecture-share.blade.php
@@ -1,29 +1,12 @@
-@extends('faculty.dashboard-menu')
+@extends('layouts.lectures')
 @section('title')- Share Lecture @endsection
 @section('current') Share Lecture @endsection
 @section('current-header') Share Lecture @endsection
-@section('manage-lectures-active') active @endsection
 @section('tab-share-active') active @endsection
 
-@section('dashboard-content')
-    <a href="{{route('lectures.index')}}" class="btn btn-primary"><i class="ti-arrow-left"></i> Go Back</a>
+@section('active-tab-content')
+    <br/>
+    <h3>Share Lecture</h3>
     <hr/>
-    @include('inc.messages')
-    <section class="section job-detail">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-9 col-sm-8">
-                    <div class="content-area">
-                        @include('lectures.lecture-tabs')
-                        <div class="box col-md-11">
-                            <br/>
-                            <h3>Share Lecture</h3>
-                            <hr/>
-                            <h1>This section is to be filled by Justin! :D</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <h1>This section is to be filled by Justin! :D</h1>
 @endsection
