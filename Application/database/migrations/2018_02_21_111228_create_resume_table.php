92e3cbf4847e8fdc1157f4e9273e17c64a1d88b3
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-03-16 23:46:09 +0800

    Implemented Resume Table Changes

--- Application/database/migrations/2018_02_21_111228_create_resume_table.php
+++ Application/database/migrations/2018_02_21_111228_create_resume_table.php
@@ -14,9 +14,18 @@ class CreateResumeTable extends Migration
     public function up()
     {
         Schema::create('resume', function (Blueprint $table) {
+            $table->integer('resume_id')->unsigned();
+            $table->integer('section_id')->unsigned();
+            $table->text('section_title');
+            $table->text('section_content');
             $table->integer('user_id')->unsigned();
-            $table->text('contents')->nullable()->default('EMPTY');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('faculty');
+
+            //composite primary key = resume_id + section_id
+            $table->primary(['resume_id','section_id']);
+            $table->index(['user_id','resume_id','section_id']);
         });
     }
 
