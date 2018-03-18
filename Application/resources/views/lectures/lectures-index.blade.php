6f84ba1ec27b891fbbc38713da2b1c1641dd0099
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-03-18 15:09:52 +0800

    Replaced Buttons with Icons to Improve UX

--- Application/resources/views/lectures/lectures-index.blade.php
+++ Application/resources/views/lectures/lectures-index.blade.php
@@ -2,27 +2,42 @@
     <h3 class="alerts-title">Manage
         Lectures <a href="{{route('lectures.create')}}" class="btn btn-success">Add a Lecture</a></h3>
     @if(count($lectures)>0)
-        <table class="table">
+        <table class="table" style="width: 100%">
+            <colgroup>
+                <col span="1" style="width: 70%;">
+                <col span="1" style="width: 15%;">
+                <col span="1" style="width: 15%;">
+            </colgroup>
             <thead class="">
             <tr>
                 <th>Lecture Title</th>
-                <th># of Files</th>
+                <th>Number of Files</th>
                 <!--<th>Assigned to</th>-->
-                <th>Action</th>
+                <th style="text-align: center">Action</th>
             </tr>
             </thead>
             <tbody>
             @foreach($lectures as $lecture)
                 <tr>
-                    <td><a href="/lectures/{{$lecture->id}}"><h3>{{$lecture->title}}</h3></a></td>
-                    <td><p class="badge">{{$lecture->files->count()}}</p></td>
+                    <td style="vertical-align: middle"><a href="/lectures/{{$lecture->id}}"><h3>{{$lecture->title}}</h3></a></td>
+                    <td style="text-align: center; vertical-align: middle"><p class="badge">{{$lecture->files->count()}}</p></td>
                     <!--<td><p>APC</p></td>-->
-                    <td style="width:90px;">
-                        <a href="/lectures/{{$lecture->id}}/edit" class="btn btn-primary btn-block" style="margin-bottom: 2px;">Update</a>
-                        {!! Form::open(['action'=>['LecturesController@destroy',$lecture->id],'method'=>'POST']) !!}
-                        {{ Form::hidden('_method','DELETE') }}
-                        {!! Form::button('Delete',['class'=>'btn btn-danger btn-block','data-toggle'=>'confirmation']) !!}
-                        {!! Form::close() !!}
+                    <td style="vertical-align: middle;">
+                        <div class="row">
+                            <div class="col-md-offset-1 col-md-5">
+                                <a href="/lectures/{{$lecture->id}}/edit" data-toggle="tooltip" title="Edit Lecture">
+                                    <img src="{{asset('img/edit.png')}}" width="30" height="30"/>
+                                </a>
+                            </div>
+                            <div class="col-md-6">
+                                {!! Form::open(['action'=>['LecturesController@destroy',$lecture->id],'method'=>'POST']) !!}
+                                {{ Form::hidden('_method','DELETE') }}
+                                <input type="image" src="{{asset('img/delete-icon.png')}}" height="30" width="30"
+                                       data-toggle="confirmation" alt="Submit Form" />
+                                {{--{!! Form::button('Delete',['class'=>'btn btn-danger btn-block','data-toggle'=>'confirmation']) !!}--}}
+                                {!! Form::close() !!}
+                            </div>
+                        </div>
                     </td>
                 </tr>
             @endforeach
