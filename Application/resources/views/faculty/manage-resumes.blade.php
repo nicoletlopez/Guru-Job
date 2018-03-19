2f45ccd5c7443abc42d146ee24c999261268f538
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-03-20 00:15:41 +0800

    added manage-resumes view

---
+++ Application/resources/views/faculty/manage-resumes.blade.php
@@ -0,0 +1,10 @@
+@extends('faculty.dashboard-menu')
+@section('title')- My Resumes @endsection
+@section('current') My Resumes @endsection
+@section('current-header') My Resumes @endsection
+@section('manage-resumes-active') active @endsection
+
+@section('dashboard-content')
+    @include('inc.messages')
+    @include('resumes.resumes-index')
+@endsection
