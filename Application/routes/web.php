8c4f6770daa3fd09f88ed4ea11a623a6abfed73a
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-03-22 14:25:50 +0800

    Revert "Download route"
    This reverts commit 7530554bcb6b26982a31d2ab9b571102215e34fa.

--- Application/routes/web.php
+++ Application/routes/web.php
@@ -86,7 +86,6 @@ Route::resource('document-spaces','DocumentSpacesController');
 //resumes
 
 Route::resource('resumes','ResumesController',['except'=>['show']]);
-Route::get('/resumes/{resume}/{template}/download','ResumesController@download')->name('resumes.download');
 Route::get('/resumes/{resume}/{template}','ResumesController@show')->name('resumes.show');
 //test
 
