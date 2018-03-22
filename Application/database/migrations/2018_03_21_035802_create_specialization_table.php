dc95072aadc7de02d30ec71e7d6108f3c221b474
Justin A. Parado <japarado@student.apc.edu.ph>
2018-03-23 00:09:54 +0800

    renamed skill migration to specialization

--- Application/database/migrations/2018_03_21_035802_create_specialization_table.php
+++ Application/database/migrations/2018_03_21_035802_create_specialization_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSkillTable extends Migration
+class CreateSpecializationTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSkillTable extends Migration
      */
     public function up()
     {
-        Schema::create('skill', function (Blueprint $table) {
+        Schema::create('specialization', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->text('desc')->nullable();
@@ -31,6 +31,6 @@ class CreateSkillTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('skill');
+        Schema::dropIfExists('specialization');
     }
 }
