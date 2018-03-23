a540b526f1201c19e856d95279671ce8eefe1f3b
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-03-23 22:44:20 +0800

    hr model, order jobs by created_at

--- Application/app/Hr.php
+++ Application/app/Hr.php
@@ -23,7 +23,7 @@ class Hr extends Model
 
     public function jobs()
     {
-        return $this->hasMany(Job::class, 'user_id', 'user_id');
+        return $this->hasMany(Job::class, 'user_id', 'user_id')->orderBy('created_at','desc');
     }
 
     public function documentSpaces()
