c35bcc840cdefa02d06549755d553774ea24e410
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-03-23 21:57:08 +0800

    commented out horizontal searchbar

--- Application/resources/views/jobs/job-listings.blade.php
+++ Application/resources/views/jobs/job-listings.blade.php
@@ -6,10 +6,12 @@
 @section('active-jobs') active @endsection
 @section('content')
     @include('inc.header')
+    <!--
     <div style="background-color:whitesmoke;">
         <br/>
         @include('inc.searchbar')
     </div>
+    -->
     @include('layouts.jobs')
 @endsection
 
