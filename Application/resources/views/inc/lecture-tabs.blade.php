ac5c613958e47dc2b82059b627488b97694358a7
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-03-24 17:13:30 +0800

    Improved Lecture Tabs UI

--- Application/resources/views/inc/lecture-tabs.blade.php
+++ Application/resources/views/inc/lecture-tabs.blade.php
@@ -1,6 +1,22 @@
-<ul class="nav nav-tabs col-md-11 col-sm-8">
-    <li class="@yield('tab-detail-active')"><a href="/lectures/{{$lecture->id}}">Lecture Details</a></li>
-    <li class="@yield('tab-file-active')"><a href="/lectures/{{$lecture->id}}/files">Lecture Files</a></li>
-    <li class="@yield('tab-assign-active')"><a href="/lectures/{{$lecture->id}}/assign">Assign Lecture</a></li>
-    <li class="@yield('tab-share-active')"><a href="/lectures/{{$lecture->id}}/share">Share Lecture</a></li>
+<ul class="nav nav-tabs">
+    <li class="@yield('tab-detail-active') lectureTabs">
+        <a href="/lectures/{{$lecture->id}}" style="text-align: center">
+            Lecture Details
+        </a>
+    </li>
+    <li class="@yield('tab-file-active') lectureTabs">
+        <a href="/lectures/{{$lecture->id}}/files" style="text-align: center">
+            Lecture Files
+        </a>
+    </li>
+    <li class="@yield('tab-assign-active') lectureTabs">
+        <a href="/lectures/{{$lecture->id}}/assign" style="text-align: center">
+            Assign Lecture
+        </a>
+    </li>
+    <li class="@yield('tab-share-active') lectureTabs">
+        <a href="/lectures/{{$lecture->id}}/share" style="text-align: center">
+            Share Lecture
+        </a>
+    </li>
 </ul>
