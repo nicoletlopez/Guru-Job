72ed8697b2184eda3317d9dc580c1cec9dfe45f1
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-03-26 21:42:22 +0800

    Created Tab Inclusion for Manage Applications

---
+++ Application/resources/views/inc/application-tabs.blade.php
@@ -0,0 +1,12 @@
+<ul class="nav nav-tabs">
+    <li class="@yield('tab-pending-active') applicationTabs">
+        <a href="/applications" style="text-align: center">
+            Pending Applications
+        </a>
+    </li>
+    <li class="@yield('tab-accepted-active') applicationTabs">
+        <a href="#" style="text-align: center">
+            Accepted Applicants
+        </a>
+    </li>
+</ul>
