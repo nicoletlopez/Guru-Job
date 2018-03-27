8067d139f7acd7aa1e10d1ae0b9a13550c3034fd
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-03-27 16:56:30 +0800

    Fixed Job Listing Format

--- Application/resources/views/layouts/jobs.blade.php
+++ Application/resources/views/layouts/jobs.blade.php
@@ -10,7 +10,7 @@
                 <h2 class="section-title">Jobs</h2>
                 @if(count($jobs)>0)
                     @foreach($jobs as $job)
-                        <div class="job-list">
+                        <div class="job-list col-md-12">
                             <div class="thumb">
                                 <a href="/jobs/{{$job->id}}"><img width="100" height=""
                                                                   src="{{$job->hr->user->profile->picture}}" alt=""></a>
@@ -18,28 +18,29 @@
                             <div class="job-list-content">
                                 <h4><a href="/jobs/{{$job->id}}">{{$job->title}}</a></h4>
                                 <p>
-                                @if($job->type == 'FT')
-                                    <span class="full-time">Full-Time</span>
-                                @else
-                                    <span class="part-time">Part-Time</span>
-                                @endif
+                                    @if($job->type == 'FT')
+                                        <span class="full-time">Full-Time</span>
+                                    @else
+                                        <span class="part-time">Part-Time</span>
+                                    @endif
                                 </p>
                                 Posted by <span style="font-weight:bold;color:#FC4A1A"
                                                 class="text-primary">{{$job->hr->user->name}}</span>
                                 <p>{!! str_limit($job->desc,500,'...') !!}</p>
                                 <div class="job-tag">
-                                    <div class="pull-left">
+                                    <div class="pull-left col-md-9">
                                         <div class="meta-tag">
                                             <!--<span><a href="#"><i class="ti-brush"></i>Art/Design</a></span>-->
                                             <span><i class="ti-location-pin"></i>{{$job->hr->user->profile->street_address}}
                                                 , {{$job->hr->user->profile->city}}</span>
+                                            <br/>
                                             {{--<span><i class="ti-time"></i>timeFrom - timeTo</span>--}}
                                             <span><i class="ti-world"></i>{{$job->jobSchedules()}}</span>
                                         </div>
                                     </div>
-
-                                        <a href="/jobs/{{$job->id}}" class="btn btn-common btn-rm">More Detail</a>
-
+                                    <div class="col-md-3">
+                                        <a href="/jobs/{{$job->id}}" class="btn btn-common btn-rm">More Details</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
