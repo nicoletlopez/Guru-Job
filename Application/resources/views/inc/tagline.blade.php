1ed3e6c7eb1f539f7f5a19628b238fcd52247330
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-03-29 02:40:20 +0800

    Added buttons in tagline

--- Application/resources/views/inc/tagline.blade.php
+++ Application/resources/views/inc/tagline.blade.php
@@ -1,3 +1,20 @@
-<h1 style="text-align: center">More Opportunities</h1>
-<br/>
-<h1 style="text-align: center">for Filipino Teachers</h1>
+<div class="row">
+    <div class="col-md-12">
+        <h1 style="text-align: center">More Opportunities</h1>
+        <br/>
+        <h1 style="text-align: center">for Filipino Teachers</h1>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <br/>
+            <div class="center">
+                @guest
+                    <a href="{{route('jobs.index')}}" class="btn btn-success">Search Jobs</a>
+                    <a href="{{route('register')}}" class="btn btn-primary">Register for Free</a>
+                @else
+                    <a href="{{route('jobs.index')}}" class="btn btn-success">Search Jobs</a>
+                @endguest
+            </div>
+        </div>
+    </div>
+</div>
