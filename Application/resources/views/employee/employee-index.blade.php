22e875975a0fbd1ce1e1f4028ab1ad9c6c610204
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-03-29 18:42:07 +0800

    Employee Index View

---
+++ Application/resources/views/employee/employee-index.blade.php
@@ -0,0 +1,8 @@
+@extends('hr.dashboard-menu')
+@section('title')- Manage Employees @endsection
+@section('current') Manage Employees @endsection
+@section('current-header') Manage Employees @endsection
+@section('manage-employees-active') active @endsection
+
+@section('dashboard-content')
+@endsection
