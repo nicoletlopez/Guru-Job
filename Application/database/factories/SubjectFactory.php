92d4517098096938e7346342ea780ac548c3310f
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-03-30 00:18:33 +0800

    Seeds for Subject Timestamps

--- Application/database/factories/SubjectFactory.php
+++ Application/database/factories/SubjectFactory.php
@@ -4,5 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Subject::class, function (Faker $faker) {
     return [
+        'created_at' => $faker->dateTime($max = 'now'),
+        'updated_at' => $faker->dateTime($max = 'now'),
     ];
 });
