2428899cc2a54d3f264d0675509dc6c7227c5757
Justin A. Parado <japarado@student.apc.edu.ph>
2018-03-31 02:06:06 +0800

    job migration revisions

--- Application/database/migrations/2018_03_21_035412_create_job_table.php
+++ Application/database/migrations/2018_03_21_035412_create_job_table.php
@@ -15,22 +15,24 @@ class CreateJobTable extends Migration
     {
         Schema::create('job', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned();
+            $table->integer('hr_id')->unsigned();
+            $table->integer('subject_id')->unsigned();
+
             $table->string('title');
-            //enable for mysql
-            //$table->string('desc')->nullable();
             $table->text('desc')->nullable();
             $table->string('type')->nullable();
             $table->double('ceiling_salary');
             $table->double('floor_salary');
             $table->timestamps();
 
-            $table->foreign('user_id')->references('user_id')->on('hr');
+            $table->foreign('hr_id')->references('user_id')->on('hr');
+            $table->foreign('subject_id')->references('id')->on('subject');
 
             //misc
             $table->softDeletes();
             $table->index('id');
             $table->index('user_id');
+            $table->index('subject_id');
             $table->index(['title','desc']);
         });
     }
