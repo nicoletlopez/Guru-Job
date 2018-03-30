f63d6b803f5a98f58e0550c9120d575d3882c38d
Justin A. Parado <japarado@student.apc.edu.ph>
2018-03-31 00:15:11 +0800

    faculty table migration

--- Application/database/migrations/2018_03_21_035535_create_subject_table.php
+++ Application/database/migrations/2018_03_21_035535_create_subject_table.php
@@ -18,11 +18,13 @@ class CreateSubjectTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->integer('job_id')->unsigned()->nullable();
             $table->string('name');
+            $table->integer('faculty_id')->unsigned()->nullable();
             $table->text('desc');
 
 
             $table->foreign('user_id')->references('user_id')->on('hr');
             $table->foreign('job_id')->references('id')->on('job');
+            $table->foreign('faculty_id')->references('user_id')->on('faculty');
 
             $table->timestamps();
             //misc
