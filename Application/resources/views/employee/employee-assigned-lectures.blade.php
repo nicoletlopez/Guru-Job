3c80e0b1d2d3feec227714adfb68684f331712ed
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-04-03 01:19:54 +0800

    Created Views for Employee Functionalities

---
+++ Application/resources/views/employee/employee-assigned-lectures.blade.php
@@ -0,0 +1,9 @@
+@extends('hr.dashboard-menu')
+@section('title')- Assigned Lectures@endsection
+@section('current') Assigned Lectures @endsection
+@section('current-header') Assigned Lectures @endsection
+@section('manage-employees-active') active @endsection
+
+@section('dashboard-content')
+
+@endsection
---
+++ Application/resources/views/employee/employee-documents.blade.php
@@ -0,0 +1,9 @@
+@extends('hr.dashboard-menu')
+@section('title')- Employee Documents@endsection
+@section('current') Employee Documents @endsection
+@section('current-header') Employee Documents @endsection
+@section('manage-employees-active') active @endsection
+
+@section('dashboard-content')
+
+@endsection
