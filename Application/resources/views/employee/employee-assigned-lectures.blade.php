76cd8631d123776eae8e4c66fb516380bc6f6228
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-04-05 23:51:48 +0800

    Transferred employee assigned lectures view to a subfolder

--- Application/resources/views/employee/employee-assigned-lectures.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('hr.dashboard-menu')
-@section('title')- Assigned Lectures@endsection
-@section('current') Assigned Lectures @endsection
-@section('current-header') Assigned Lectures @endsection
-@section('manage-employees-active') active @endsection
-
-@section('dashboard-content')
-
-@endsection
