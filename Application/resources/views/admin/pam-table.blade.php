bf231a9402af70e4bb3a0ce2dad26f522d6a55f7
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-04-07 02:04:29 +0800

    pam

---
+++ Application/resources/views/admin/pam-table.blade.php
@@ -0,0 +1,33 @@
+@extends('base')
+@section('content')
+
+    <div class="job-alerts-item candidates">
+        <h3 class="alerts-title">
+            Subscriptions
+        </h3>
+        <table class="table" style="width: 50%">
+
+                <thead>
+                <tr>
+                    <th>School</th>
+                    <th>Balance</th>
+                    <th>Status</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td style="vertical-align: middle">
+                            APC
+                        </td>
+                        <td style="vertical-align: middle">
+                            10000
+                        </td>
+                        <td style="vertical-align: middle">
+                            aw
+                        </td>
+                    </tr>
+                </tbody>
+        </table>
+    </div>
+
+    @endsection
