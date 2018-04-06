47f9819f5e3860b4cff7a13df6bd5141432f3540
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-04-07 02:30:44 +0800

    if statement wow

--- Application/resources/views/notifications/notifications-index.blade.php
+++ Application/resources/views/notifications/notifications-index.blade.php
@@ -7,16 +7,23 @@
 @section('dashboard-content')
     <div class="job-alerts-item notification">
         <h3 class="alerts-title">Your Notifications</h3>
+        <hr/>
+        @if(count($notifications)>0)
         @foreach($notifications as $key=>$notification)
             <div class="notification-item">
                 <div class="thums">
-                    <img src="{{$images[$key]}}"/>
+                    <img class="img-responsive" src="{{$images[$key]}}"/>
+
                 </div>
                 <div class="text-left">
-                    <p><b>{{$notification->data['hr']['name']}}</b> {{$notification->data['message']}}</p>
+                    <p><b>{{$notification->data['hr']['name']}}</b></p>
+                    <p> {{$notification->data['message']}}</p>
                     <!--<span class="time"><i class="ti-time"></i>3 Hours ago</span>-->
                 </div>
             </div>
         @endforeach
+            @else
+            <h4>No Notifications</h4>
+            @endif
     </div>
 @endsection
