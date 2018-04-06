2dbd8deb4c393c70327b6b09bfe6d4b607e3df79
Justin A. Parado <japarado@student.apc.edu.ph>
2018-04-07 03:08:25 +0800

    billing for HR

--- Application/config/services.php
+++ Application/config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => App\User::class,
+        'model' => App\Hr::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
