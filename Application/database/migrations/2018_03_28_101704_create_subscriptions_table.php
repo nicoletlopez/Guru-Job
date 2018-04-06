5c0c2d302d33ef81d6c9454bead430ceec8b667e
Justin A. Parado <japarado@student.apc.edu.ph>
2018-04-07 01:43:30 +0800

    fixed typo in migration

--- Application/database/migrations/2018_03_28_101704_create_subscriptions_table.php
+++ Application/database/migrations/2018_03_28_101704_create_subscriptions_table.php
@@ -46,6 +46,6 @@ class CreateSubscriptionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('subscriptions');
+        Schema::dropIfExists('subscription');
     }
 }
