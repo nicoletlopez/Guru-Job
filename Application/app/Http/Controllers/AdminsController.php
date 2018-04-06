bd9843b4af3a864686cea65afe5d3747e50c8854
Justin A. Parado <japarado@student.apc.edu.ph>
2018-04-07 02:10:20 +0800

    auth contscuctor

--- Application/app/Http/Controllers/AdminsController.php
+++ Application/app/Http/Controllers/AdminsController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class AdminsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
