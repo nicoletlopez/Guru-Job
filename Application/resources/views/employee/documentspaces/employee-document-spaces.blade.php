5c126c95a3a81ad6ab8ac840f4a40766ef9a0ca1
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-04-06 20:19:30 +0800

    View for employee assigned document spaces transferred to sub folder

---
+++ Application/resources/views/employee/documentspaces/employee-document-spaces.blade.php
@@ -0,0 +1,34 @@
+@extends('hr.dashboard-menu')
+@section('title')- Employee Document Spaces @endsection
+@section('current') Employee Document Spaces @endsection
+@section('current-header') Employee Document Spaces @endsection
+@section('manage-employees-active') active @endsection
+
+@section('dashboard-content')
+    <div class="job-alerts-item candidates">
+        <h3 class="alerts-title">Employee Document Spaces</h3>
+        <hr/>
+        <div class="row">
+            <div class="col-md-12">
+                @if(count($documentSpaces)>0)
+                    @foreach($documentSpaces as $documentSpace)
+                        <a href="/employees/{{$faculty_id}}/document-spaces/{{$documentSpace->id}}">
+                            <div style="" class="col-md-3 col-sm-3 col-xs-12 f-category">
+                                {!! Form::close() !!}
+                                <div class="icon">
+                                    <i class="ti-folder"></i>
+                                </div>
+                                <div style="word-wrap:break-word;" class="">
+                                    <h3>{!! str_limit($documentSpace->title,28,'...') !!}</h3>
+                                </div>
+                                <span class="badge">{{count($documentSpace->documents)}} Files</span>
+                            </div>
+                        </a>
+                    @endforeach
+                @else
+                    <h5>No Folders.</h5>
+            @endif
+            </div>
+        </div>
+    </div>
+@endsection
--- Application/resources/views/employee/employee-documents.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('hr.dashboard-menu')
-@section('title')- Employee Documents@endsection
-@section('current') Employee Documents @endsection
-@section('current-header') Employee Documents @endsection
-@section('manage-employees-active') active @endsection
-
-@section('dashboard-content')
-
-@endsection
