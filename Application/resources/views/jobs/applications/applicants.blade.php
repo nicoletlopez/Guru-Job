e8908836e78b95cb28536ec980d8a76179577c49
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-04-08 15:48:25 +0800

    Imporved Search bar and added resume link to applications per job

--- Application/resources/views/jobs/applications/applicants.blade.php
+++ Application/resources/views/jobs/applications/applicants.blade.php
@@ -14,8 +14,13 @@
             <div class="col-md-5">
                 {!! Form::open(['action'=>["ApplicationsController@search",$id],'method'=>'GET']) !!}
                 {{--<input class="form-control" type="text" name="s" placeholder="job title / keywords">--}}
-                {{Form::text('search-term','',['class'=>'form-control','placeholder'=>'Search Applicants',
-                                                'style' => 'height:30px'])}}
+                <div class="form-group">
+                    <div class="input-icon">
+                        <i class="ti-search"></i>
+                        <input id="search-term" type="text" class="form-control"
+                               placeholder="Search Applicants" name="search-term" style="height: 45px;">
+                    </div>
+                </div>
                 {!! Form::close() !!}
             </div>
             <div class="col-md-3">
@@ -33,7 +38,13 @@
         @foreach($applicants as $key=>$applicant)
             <div class="box col-md-12">
                 <div class="col-md-1">
-                    <b style="font-size: 20px">#{{++$key}}</b>
+                    <p style="margin-bottom: 15px">
+                        <b style="font-size: 20px;">#{{++$key}}</b>
+                    </p>
+                    <a target="_blank"
+                       href="/applications/{{$applicant->user_id}}/resume/{{$applicant->mainTemplate($applicant->user_id)}}">
+                        <span class="ti-envelope" style="font-size: 40px" title="Resume"></span>
+                    </a>
                 </div>
                 <div class="col-md-2">
                     <img src="{{$applicant->user->profile->picture}}" height="80" style="border-radius: 8px;"/>
@@ -48,7 +59,7 @@
                 <div class="col-md-4">
                     <b style="font-size: 14px">Date Applied:</b><br/>
                     <b style="font-size: 14px">{{date('F j, Y \a\t g:i a', strtotime($applicant->pivot->created_at))}}</b><br/>
-{{--                    <b style="font-size: 12px">{{date('g:i a', strtotime($applicant->pivot->created_at))}}</b>--}}
+                    {{--                    <b style="font-size: 12px">{{date('g:i a', strtotime($applicant->pivot->created_at))}}</b>--}}
                     <br/>
 
                     <a href="/applications/{{$applicant->pivot->job_id}}/{{$applicant->pivot->user_id}}/edit"
