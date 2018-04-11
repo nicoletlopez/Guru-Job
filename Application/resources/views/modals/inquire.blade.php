18171162b6235f7396e4823523b74b35676d6a02
Justin A. Parado <japarado@student.apc.edu.ph>
2018-04-12 01:37:20 +0800

    redefine to laravel field

--- Application/resources/views/modals/inquire.blade.php
+++ Application/resources/views/modals/inquire.blade.php
@@ -8,120 +8,122 @@
                 </button>
                 <h3 class="modal-title" id="myModalLabel">Subscription form</h3>
             </div>
-            <form class="login-form" method="POST" action="{{ route('register') }}">
-                <div class="modal-body">
-                    {{ csrf_field() }}
-                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                        <div class="row">
-                            <div class="col-md-2">
-                                <label class="pull-left" for="name" style="font-size: 16px; padding-top: 15px">Full Name: </label>
-                            </div>
-                            <div class="col-md-10">
-                                <div class="input-icon">
-                                    <i class="ti-user"></i>
-                                    <input id="name" type="text" class="form-control" name="name"
-                                           value="{{ old('name') }}" required autofocus>
+            {!! Form::open(['action'=>'SubscriptionsController@store','class'=>'form-ad','method'=>'POST']) !!}
+            {{--<form class="login-form" method="POST" action="SubscriptionsController@store">--}}
+            <div class="modal-body">
+                {{ csrf_field() }}
+                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <label class="pull-left" for="name" style="font-size: 16px; padding-top: 15px">Full
+                                Name: </label>
+                        </div>
+                        <div class="col-md-10">
+                            <div class="input-icon">
+                                <i class="ti-user"></i>
+                                <input id="name" type="text" class="form-control" name="name"
+                                       value="{{ old('name') }}" required autofocus>
 
-                                    @if ($errors->has('name'))
-                                        <span class="help-block">
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
-                                    @endif
-                                </div>
+                                @endif
                             </div>
                         </div>
                     </div>
-                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <div class="row">
-                            <div class="col-md-2">
-                                <label class="pull-left" for="email" style="font-size: 16px; padding-top: 15px">
-                                    E-mail:
-                                </label>
-                            </div>
-                            <div class="col-md-10">
-                                <div class="input-icon">
-                                    <i class="ti-email"></i>
-                                    <input id="email" type="email" class="form-control" name="email"
-                                           value="{{ old('email') }}" required>
+                </div>
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <label class="pull-left" for="email" style="font-size: 16px; padding-top: 15px">
+                                E-mail:
+                            </label>
+                        </div>
+                        <div class="col-md-10">
+                            <div class="input-icon">
+                                <i class="ti-email"></i>
+                                <input id="email" type="email" class="form-control" name="email"
+                                       value="{{ old('email') }}" required>
 
-                                    @if ($errors->has('email'))
-                                        <span class="help-block">
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                    @endif
-                                </div>
+                                @endif
                             </div>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col-md-2">
-                                <label class="pull-left" for="credit_card" style="font-size: 16px; padding-top: 15px">
-                                    Credit Card:
-                                </label>
+                </div>
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <label class="pull-left" for="credit_card" style="font-size: 16px; padding-top: 15px">
+                                Credit Card:
+                            </label>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="input-icon">
+                                <i class="ti-credit-card"></i>
+                                <input id="password-confirm" type="text" class="form-control"
+                                       name="credit_card" required>
                             </div>
-                            <div class="col-md-4">
-                                <div class="input-icon">
-                                    <i class="ti-credit-card"></i>
-                                    <input id="password-confirm" type="text" class="form-control"
-                                           name="credit_card" required>
-                                </div>
-                            </div>
-                            <div class="col-md-2">
-                                <label class="pull-left" for="expiry_date" style="font-size: 16px; padding-top: 15px">
-                                    Expiry Date:
-                                </label>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="input-icon">
-                                    <i class="ti-calendar"></i>
-                                    <input id="expiry-date" type="text" class="form-control"
-                                           placeholder="Expiry Date" name="expiry_date" required>
-                                </div>
+                        </div>
+                        <div class="col-md-2">
+                            <label class="pull-left" for="expiry_date" style="font-size: 16px; padding-top: 15px">
+                                Expiry Date:
+                            </label>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="input-icon">
+                                <i class="ti-calendar"></i>
+                                <input id="expiry-date" type="date" class="form-control"
+                                       placeholder="Expiry Date" name="expiry_date" required>
                             </div>
                         </div>
                     </div>
-                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                        <div class="row">
-                            <div class="col-md-2">
-                                <label class="pull-left" for="password" style="font-size: 16px; padding-top: 15px">
-                                    Password:
-                                </label>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="input-icon">
-                                    <i class="ti-lock"></i>
-                                    <input id="password" type="password" placeholder="Password" class="form-control"
-                                           name="password" required>
+                </div>
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <label class="pull-left" for="password" style="font-size: 16px; padding-top: 15px">
+                                Password:
+                            </label>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="input-icon">
+                                <i class="ti-lock"></i>
+                                <input id="password" type="password" placeholder="Password" class="form-control"
+                                       name="password" required>
 
-                                    @if ($errors->has('password'))
-                                        <span class="help-block">
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                    @endif
-                                </div>
+                                @endif
                             </div>
-                            <div class="col-md-2">
-                                <label class="pull-left" for="password-confirm" style="font-size: 16px; padding-top: 15px">
-                                    Confirm:
-                                </label>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="input-icon">
-                                    <i class="ti-lock"></i>
-                                    <input id="password-confirm" type="password" class="form-control"
-                                           placeholder="Confirm password" name="password_confirmation" required>
-                                </div>
+                        </div>
+                        <div class="col-md-2">
+                            <label class="pull-left" for="password-confirm" style="font-size: 16px; padding-top: 15px">
+                                Confirm:
+                            </label>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="input-icon">
+                                <i class="ti-lock"></i>
+                                <input id="password-confirm" type="password" class="form-control"
+                                       placeholder="Confirm password" name="password_confirmation" required>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-common log-btn">Subscribe Now!</button>
-                </div>
-            </form>
-
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-common log-btn">Subscribe Now!</button>
+            </div>
+            {{--</form>--}}
+        {!! Form::close() !!}
         </div>
     </div>
 </div>
