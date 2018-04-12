470676026b23c7e582d0a57ac54fe6ef69b100ae
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-04-12 21:54:39 +0800

    d pa tapos

--- Application/resources/views/inc/profile-info.blade.php
+++ Application/resources/views/inc/profile-info.blade.php
@@ -2,29 +2,35 @@
     @if(Auth::user()->type == 'HR')
         <div class="row">
 
-                <div class="thumb col-md-3">
-                    <img class="img-rounded center-block" height="128" width="128" src="{{$profile->picture}}" alt="">
+            <div class="thumb col-md-3">
+                <img class="img-rounded center-block" height="128" width="128" src="
+                @if(isset($profile->picture))
+                {{$profile->picture}}
+                @elseif(!($profile->picture))
+                {{asset('img/default-user.png')}}
+                @endif
+                        " alt="">
+            </div>
+            <div class="col-md-9">
+                <h3>{{$user->name}}<a href="#" data-target=".edit-personal-details" data-toggle="modal"><i
+                                class="ti-pencil"></i></a></h3>
+                <p class="sub-title">Established on: {{$date}}</p>
+                <p><span class="address"><i class="ti-home"></i> {{$profile->street_address}}</span></p>
+                <p><span class="address"><i class="ti-location-pin"></i> {{$profile->city}}</span></p>
+                <p><span class="address"><i class="ti-mobile"></i> {{$profile->contact_number}}</span></p>
+                <!--
+                <div class="social-link">
+                    <a class="twitter" target="_blank" data-original-title="twitter" href="#"
+                       data-toggle="tooltip" data-placement="top"><i class="fa fa-twitter"></i></a>
+                    <a class="facebook" target="_blank" data-original-title="facebook" href="#"
+                       data-toggle="tooltip" data-placement="top"><i class="fa fa-facebook"></i></a>
+                    <a class="google" target="_blank" data-original-title="google-plus" href="#"
+                       data-toggle="tooltip" data-placement="top"><i class="fa fa-google"></i></a>
+                    <a class="linkedin" target="_blank" data-original-title="linkedin" href="#"
+                       data-toggle="tooltip" data-placement="top"><i class="fa fa-linkedin"></i></a>
                 </div>
-                <div class="col-md-9">
-                    <h3>{{$user->name}}<a href="#" data-target=".edit-personal-details" data-toggle="modal"><i
-                                    class="ti-pencil"></i></a></h3>
-                    <p class="sub-title">Established on: {{$date}}</p>
-                    <p><span class="address"><i class="ti-home"></i> {{$profile->street_address}}</span></p>
-                    <p><span class="address"><i class="ti-location-pin"></i> {{$profile->city}}</span></p>
-                    <p><span class="address"><i class="ti-mobile"></i> {{$profile->contact_number}}</span></p>
-                    <!--
-                    <div class="social-link">
-                        <a class="twitter" target="_blank" data-original-title="twitter" href="#"
-                           data-toggle="tooltip" data-placement="top"><i class="fa fa-twitter"></i></a>
-                        <a class="facebook" target="_blank" data-original-title="facebook" href="#"
-                           data-toggle="tooltip" data-placement="top"><i class="fa fa-facebook"></i></a>
-                        <a class="google" target="_blank" data-original-title="google-plus" href="#"
-                           data-toggle="tooltip" data-placement="top"><i class="fa fa-google"></i></a>
-                        <a class="linkedin" target="_blank" data-original-title="linkedin" href="#"
-                           data-toggle="tooltip" data-placement="top"><i class="fa fa-linkedin"></i></a>
-                    </div>
-                    -->
-                </div>
+                -->
+            </div>
 
         </div>
         <div class="about-me item">
@@ -36,7 +42,12 @@
         @if(Auth::user()->id == $profile->user_id)
             <div class="row">
                 <div class="thumb col-md-3">
-                    <img class="img-rounded center-block" height="128" width="128" src="{{$profile->picture}}" alt="">
+                    <img class="img-rounded center-block" height="128" width="128" src="
+                    @if(isset($profile->picture))
+                    {{$profile->picture}}
+                    @elseif(!($profile->picture))
+                    {{asset('img/default-user.png')}}
+                    @endif" alt="">
                 </div>
                 <div class="col-md-9">
                     <h3>{{$user->name}} <a href="#" data-target=".edit-personal-details" data-toggle="modal"><i
@@ -64,8 +75,8 @@
                 </h3>
                 {!! $profile->description !!}
             </div>
-        @endif
     @endif
+@endif
 
 <!--
     <div class="work-experence item">
